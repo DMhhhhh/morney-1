86b9f16fb600515df55f5de481e7f0e1d3ca0a3c
DMhhhh <murray-y@hotmail.com>
2022-09-22 03:03:07 +0800

    添加active-class;更新meta:vp

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="nav">
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <icon name="money"></icon>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <icon name="label"></icon>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <icon name="statistic"></icon>
       统计
     </router-link>
