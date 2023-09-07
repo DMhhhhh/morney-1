4eeaa368e798952484df2153960e2acd635eb5de
DMhhhhh <git@github.com:DMhhhhh/morney-1.git>
2023-09-08 07:01:20 +0800

    新增EditLabel组件并修改路由配置

--- src/router/index.ts
+++ src/router/index.ts
@@ -26,7 +26,7 @@ const routes: Array<RouteConfig> = [
     component: Statistics
   },
   {
-    path:'/edit',
+    path:'/edit/:id',
     component: EditItem
   },
   {
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,10 +7,21 @@
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import tagListModel from '@/models/tagListModel';
 
 @Component
 export default class EditItem extends Vue {
-
+  created () {
+    const id = this.$route.params.id
+    tagListModel.fetch
+    const tags = tagListModel.tag
+    const tag = tags.find(tags=> tags.id === id)
+    if (tag) {
+      console.log(id);
+    }else {
+      this.$router.replace('/404')
+    }
+  }
 }
 </script>
 
