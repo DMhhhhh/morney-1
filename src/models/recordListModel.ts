cadd8e6df3178e58bad92c286041f5daeb079101
DMhhhhh <git@github.com:DMhhhhh/morney-1.git>
2023-09-18 04:52:03 +0800

    将create封装到recorcdListModel

--- src/models/recordListModel.ts
+++ src/models/recordListModel.ts
@@ -1,17 +1,18 @@
+import clone from '@/lib/clone';
+
 const localstorageKeyName = 'recordList';
 const recordListModel = {
   data: [] as RecordItem[],
 
-  clone(data: RecordItem[] | RecordItem) {
-    this.data = JSON.parse(JSON.stringify(data));
-    return this.data;
-  },
   fetch() {
     return JSON.parse(window.localStorage.getItem(localstorageKeyName) || '[]') as RecordItem[];
   },
-  // create() {
-  //
-  // }
+  create(record: RecordItem) {
+    const record2 = clone(record);
+    record2.createdAt = new Date();
+    this.data.push(record2);
+    this.save();
+  },
   save() {
     window.localStorage.setItem(localstorageKeyName, JSON.stringify(this.data));
   },
